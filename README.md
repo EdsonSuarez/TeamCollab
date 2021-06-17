3d859a51293eebbacf577b34e01dc49de2c3695a
EdsonSuarez <edsondz43@gmail.com>
2021-06-17 13:45:16 -0500

    add structure backend and frontend

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TeamCollab
 plataforma para la metodología scrum, lo cual consiste en una sistema que permita a los usuarios por medio de un tablero, llevar un seguimiento y planeación de sus futuros proyectos de forma fácil y dinámica.
+
+## By  CSS - CodeSoft Solution
+
+### Development team
+
+```
+Cristian Andres Gomez Monsalve
+Edson Daniel Suarez Hernandez
+Johan Stiven Quiroga
+Miguel Angel Cerquera Rodriguez
+Natalia Ortiz Maldonado
+```
---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("./db/db");
+
+require("dotenv").config();
+
+const Auth = require("./routes/auth");
+const Board = require("./routes/board");
+const Project = require("./routes/project");
+const Role = require("./routes/role");
+const Task = require("./routes/task");
+const Team = require("./routes/team");
+const User = require("./routes/user");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+//app.use("/api/auth/", Auth);
+//app.use("/api/board/", Board);
+//app.use("/api/project", Project);
+//app.use("/api/role/", Role);
+//app.use("/api/task/", Task);
+//app.use("/api/team/", Team);
+//app.use("/api/user/", User);
+
+app.listen(process.env.PORT, () =>
+  console.log("Backend server running on port: " + process.env.PORT)
+);
+
+dbConnection();
