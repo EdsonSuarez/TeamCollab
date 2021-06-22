aaa96656ce43e887db61aab300dd214fbd9a0199
NataliaOrtizMa <nataliaortizma@gmail.com>
2021-06-22 14:32:26 -0500

    Feat home component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
 import { LoginComponent } from './home/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-=======
 import { HomeComponent } from './home/home/home.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< Updated upstream
-    LoginComponent
-=======
+    LoginComponent,
     HomeComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
